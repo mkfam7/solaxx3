3195e174a67e1feae34064e27bf38abaa1838bd1
phdfam <164750558+phdfam@users.noreply.github.com>
2024-04-09 18:08:25 +0300

    Feature/updates (#7)
    * initial
    
    * update versions
    
    * Updated table schemas
    
    * Integrated updated version of solaxx3
    
    * Renamed a function in solaxx3 class
    
    * Modified a line as part of review
    
    ---------
    
    Co-authored-by: Administrator <admin@elijah.local>

--- example.py
+++ example.py
@@ -1,13 +1,10 @@
-from solaxx3.rs485 import SolaxX3
+from solaxx3.solaxx3 import SolaxX3
 
 # adjust the serial port and baud rate as necessary
 s = SolaxX3(port="/dev/ttyUSB0", baudrate=115200)
 
 if s.connect():
     s.read_all_registers()
-
-    print(s._input_registers_values_list)
-    exit()
 
     available_stats = s.list_register_names()
     for stat in available_stats:
--- src/solaxx3/__init__.py
+++ src/solaxx3/__init__.py
@@ -1,2 +1,2 @@
 # Version of the Solax RTU package
-__version__ = "0.0.6"
+__version__ = "1.0.0"
